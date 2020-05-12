18e43e625517773f2bcfa82d20a6129c747e568f
noashabtay <noashab@post.bgu.ac.il>
2020-05-12 19:04:43 +0300

    trying to pull

--- src/Presentation/mainPresentation.java
+++ src/Presentation/mainPresentation.java
@@ -3,19 +3,39 @@ package Presentation;
 import Presentation.Guest.GuestMenu;
 import Presentation.SystemManager.SystemManagerMenu;
 import Exception.*;
-
 import javax.swing.*;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 import static com.sun.glass.ui.Cursor.setVisible;
+
+
+import static DataBase.DBConnector.getConnection;
 
 public class mainPresentation {
     public static void main(String[] args){
 
+        try{
+            Connection myConn = getConnection();
+            Statement myStatement = myConn.createStatement();
+            ResultSet rs = myStatement.executeQuery("select * from League");
+
+            while (rs.next()){
+                System.out.println(rs.getString("seasonsId"));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
 //        SystemManagerMenu systemManagerMenu=new SystemManagerMenu();
 //        systemManagerMenu.showMenu();
 
-        GuestMenu guestMenu = new GuestMenu();
-        guestMenu.showMenu();
+       // GuestMenu guestMenu = new GuestMenu();
+    //    guestMenu.showMenu();
 
       // systemManagerMenu.addSystemManager();
       //  systemManagerMenu.addAssociationDeligate();
